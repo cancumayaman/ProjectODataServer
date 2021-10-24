4236e2dd8a090b6a5b016006fc96c719628dc953
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-10-24 12:43:50 +0300

    Controllers code added to StringBuilder for generate dynamically

--- src/ProjectODataServer/ProjectODataServer/Startup.cs
+++ src/ProjectODataServer/ProjectODataServer/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +20,7 @@ using Sample.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectODataServer
@@ -86,6 +89,44 @@ namespace ProjectODataServer
             odataBuilder.EntitySet<Category>("Category");
             odataBuilder.EntitySet<Product>("Product");
             return odataBuilder.GetEdmModel();
+        }
+    }
+
+    public class GenerþcTypeControllerFeatureProvider : IApplicationFeatureProvider<ControllerFeature>
+    {
+        public void PopulateFeature(IEnumerable<ApplicationPart> parts, ControllerFeature feature)
+        {
+            var types = new[] { typeof(Product), typeof(Category) };
+            var sb = new StringBuilder();
+
+            sb.AppendLine(@"using Microsoft.AspNet.OData;
+            using Microsoft.AspNetCore.Mvc;
+            using Sample.Data.DbContexts;
+            using Sample.Data.Entities;
+            using System.Linq;
+            namespace ProjectODataServer.Controllers.OData
+            {");
+
+            foreach (var type in types)
+            {
+                sb.AppendLine($@"public class {type.Name}Controller : ControllerBase
+	        {{
+	        	private readonly SampleDataDbContext _db;
+	        	public {type.Name}Controller(SampleDataDbContext db)
+	        	{{
+	        		_db = db;
+	        	}}
+	        	[EnableQuery]
+	        	public IQueryable<{type.Name}> Get()
+	        	{{
+	        		return _db.Set<{type.Name}>();
+	        	}}
+	        }}
+"           );
+            }
+
+            sb.AppendLine("}");
+
         }
     }
 }
